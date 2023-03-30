1ff3bdf750b2051a0645941105bcaf4c3c344bb9
Norm Evangelista <norm.evangelista@gmail.com>
2023-03-30 13:47:03 -0700

    Simplified Moore neighborhood code

--- MooreNeighbor.cpp
+++ MooreNeighbor.cpp
@@ -26,21 +26,21 @@ namespace gol {
 ///
 NeighborArray mooreNeighborhood(int x, int y, int gridWidth, int gridHeight, bool wrapped) {
     NeighborArray neighbors;
-    for (auto i = -1; i <= 1; i++) {
-        for (auto j = -1; j <= 1; j++) {
-            auto xCoord = x + i;
-            auto yCoord = y + j;
-            if (wrapped && (xCoord < 0 || xCoord == gridWidth || yCoord < 0 || yCoord == gridHeight)
-                && (i != 0 || j != 0)) {
-                neighbors.emplace_back(std::make_pair(xCoord, yCoord));
-            } else if (
-                    xCoord >= 0 && xCoord < gridWidth && yCoord >= 0 && yCoord < gridHeight
-                    && (i != 0 || j != 0)) {
-                neighbors.emplace_back(std::make_pair(xCoord, yCoord));
+    for (auto i = -1; i <= 1; ++i) {
+        for (auto j = -1; j <= 1; ++j) {
+            if (i != 0 || j != 0) {
+                auto xCoord = x + i;
+                auto yCoord = y + j;
+                if (wrapped) {
+                    xCoord = (xCoord + gridWidth) % gridWidth;
+                    yCoord = (yCoord + gridHeight) % gridHeight;
+                }
+                if (xCoord >= 0 && xCoord < gridWidth && yCoord >= 0 && yCoord < gridHeight) {
+                    neighbors.emplace_back(xCoord, yCoord);
+                }
             }
         }
     }
     return neighbors;
 }
-
 }  // namespace gol
