6075adb62432a871acfcfb901b40f31c99e58474
Norm Evangelista <norm@vastspace.com>
2025-10-09 13:33:15 -0700

    Updated copyright to 2025

--- ConwayGrid.cpp
+++ ConwayGrid.cpp
@@ -1,6 +1,6 @@
 /// \file ConwayGrid.cpp
 /// \author Norm Evangelista
-/// \copyright (c) 2023 Norm Evangelista
+/// \copyright (c) 2025 Norm Evangelista
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
